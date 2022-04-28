35640c3e09f6010c6e84edce49a8d9ac0c458666
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-04-28 00:39:39 -0400

    quals + skills

--- src/views/QualificationsView.vue
+++ src/views/QualificationsView.vue
@@ -1,5 +1,82 @@
 <template>
   <div class="qualifications">
-    <h1>qualifications</h1>
+    <VueWriter
+      :array="['Qualifications']"
+      :iterations="1"
+      :typeSpeed="150"
+      class="Header"
+    />
+    <div class="flexParent">
+      <div class="flexItem">
+        <div class="itemTitle">Google IT Support</div>
+        <div class="itemSubtitle">Google</div>
+        <a
+          href="https://www.coursera.org/account/accomplishments/specialization/certificate/QLNHNK4M9SSX"
+          class="itemLink"
+          >Link🔗</a
+        >
+      </div>
+      <div class="flexItem">
+        <div class="itemTitle">Programming With Google Go</div>
+        <div class="itemSubtitle">UC Irvine</div>
+        <a
+          href="https://www.coursera.org/account/accomplishments/specialization/certificate/CL6PLTXKVF9H"
+          class="itemLink"
+          >Link🔗</a
+        >
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.Header {
+  margin-top: 5%;
+  margin-bottom: 2%;
+}
+.flexParent {
+  display: flex;
+  justify-content: center;
+  gap: 6em;
+  margin-top: 5em;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.flexItem {
+  position: relative;
+  color: rgb(152, 152, 152);
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 15em;
+  height: 12em;
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 5px;
+  background-color: rgb(49, 49, 49);
+  padding: 0.5em;
+}
+
+.itemTitle {
+  font-size: 1.3em;
+  color: var(--tertiary);
+  margin-bottom: 1em;
+}
+.itemSubtitle {
+  font-size: 1.1em;
+  margin-bottom: 0.4em;
+}
+
+.flexItem > div {
+  text-align: center;
+}
+
+.itemLink {
+  color: var(--secondary);
+  position: absolute;
+  bottom: 1.2em;
+  padding: 3px 6px 3px 7px;
+  border: 1px solid var(--secondary);
+  border-radius: 5px;
+}
+</style>
